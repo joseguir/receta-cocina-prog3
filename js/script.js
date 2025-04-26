aac1e64e8d8b6aecd5dd7eee118718cccf174282
Jose <joseaguirre0095@gmail.com>
2025-04-26 00:29:23 -0300

    menu responsive

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ const btn = document.getElementById('darkModeBtn');
 btn.addEventListener('click', () => {
    document.body.classList.toggle('dark-mode');
 });
+
+ const menuToggle = document.getElementById('menuToggle');
+ const menuList = document.getElementById('menuList');
+
+  menuToggle.addEventListener('click', () => {
+    menuList.classList.toggle('show');
+  });
